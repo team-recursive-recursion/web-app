c4d925cc2e35d76749b7d5d107a2017d2446587e
Tiaan Nel <tiaannel26@gmail.com>
2018-05-09 08:10:54 +0200

    Add working map

--- WebApplication/ClientApp/app/components/home/home.component.ts
+++ WebApplication/ClientApp/app/components/home/home.component.ts
@@ -10,12 +10,17 @@ import { GeoJSON } from 'geojson'
 export class HomeComponent {
     @ViewChild(MapperComponent) public map: MapperComponent;
     url: any;
-    drag: boolean = true;
-    path: string;
-    polygon: GeoJSON;
-    multiPolygons: GeoJSON;
-    poly: GeoJSON;
-    addedPolygons: GeoJSON;
+    courseId: string = "";
+
+    terrainTypes = [
+        {"typeName": 'Rough', "ttype": 0},
+        {"typeName": 'Fairway', "ttype": 1},
+        {"typeName": 'Green', "ttype": 2},
+        {"typeName": 'Bunker', "ttype": 3},
+        {"typeName": 'Water Hazard', "ttype": 4}
+    ];
+
+    courses: golfCourses[] = [];
 
     ngAfterViewInit() {
         this.getServerURL();
@@ -33,6 +38,51 @@ export class HomeComponent {
         this.map.toggleDraggable();
     }
 
+    public changePolyType(bool: boolean, index: number) {
+        this.map.changePolyType(bool, index);
+    }
+
     constructor() {
     }
+    
+    public newPolygon() {
+        this.map.newPolygon();
+    }
+
+    public createCourse(courseName: string) {
+        if (courseName != "") {
+            console.log("Course Name: " + courseName);
+            //TODO call API
+            this.map.createCourse(courseName);
+        }
+    }
+
+    public saveCourse() {
+        //TODO
+        this.map.saveCourse();
+    }
+
+    public loadCourse(index: number) {
+        //TODO this should populate the select with courses
+        console.log(index);
+        this.map.loadCourse(index);
+    }
+
+    public loadCourses() {
+
+    }
+
+    public resetMap() {
+        this.map.resetMap();
+    }
+}
+
+interface golfCourses {
+    courseName: string;
+    courseId: string;
+}
+
+interface golfCourse {
+    courseId: string;
+
 }
--- WebApplication/ClientApp/app/components/mapper/mapper.component.ts
+++ WebApplication/ClientApp/app/components/mapper/mapper.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MouseEvent, LatLngLiteral } from '@agm/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-mapper',
@@ -7,6 +10,8 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MapperComponent implements OnInit {
+    map: any;
+
     title: string = "mapping";
     lat: number = -25.658712;
     lng: number = 28.140347;
@@ -14,22 +19,144 @@ export class MapperComponent implements OnInit {
     mapType: string = "satellite";
     mapDraggable: boolean = true;
 
+    //current polygon
+    poly: polygon = {
+        strokeOpacity: 1,
+        strokeWeight: 5,
+        fillOpacity: 0.4,
+        fillColor: this.getColor(0),
+        strokeColor: this.getColor(0),
+        polyDraggable: true,
+        editable: true,
+        clickable: true,
+        terrainType: 0,
+        zIndex: 0,
+        paths: []
+    }
+    //all polygons
+    polygons: polygon[] = [];
+
     constructor() {
         //TODO
     }
 
     ngOnInit() {
+        this.poly = this.defaultPolygon();
     }
 
     ngAfterViewInit() {
+    }
+
+    public mapClicked($event: MouseEvent) {
+        this.poly.paths.push({
+            lat: $event.coords.lat,
+            lng: $event.coords.lng
+        });
+    }
+
+    public updatePolyPosition(_poly: polygon, $event: MouseEvent) {
+        console.log("polygon:", _poly," ", "whatever:", $event);
+    }
+
+    public newPolygon() {
+        //    this.poly.polyDraggable = false;
+        //this.poly.editable = false;
+        //this.poly.clickable = false;
+        this.polygons.push(this.poly);
+        this.poly = this.defaultPolygon();
+    }
+
+    public saveCourse() {
+
+    }
+
+    public loadCourses(index: number) {
+
+    }
+
+    public loadCourse(index: number) {
+
+    }
+
+    public resetMap() {
+
     }
 
     public toggleDraggable() {
         this.mapDraggable = !this.mapDraggable;
     }
 
-    public addPolygons() {
-    }
     public getPolygons() {
     }
+
+    public changePolyType(bool: boolean, index: number) {
+        this.poly.fillColor = this.getColor(index);
+        this.poly.strokeColor = this.getColor(index);
+        this.poly.polyDraggable = bool;
+        this.poly.editable = bool;
+        this.poly.clickable = bool;
+        this.poly.terrainType = index;
+        this.poly.zIndex = index;
+    }
+
+    public resetMap() {
+    }
+
+    private addLatLng(event: object) {
+    }
+
+    private error(message: string) {
+    }
+
+    private getColor(data: number): string {
+        console.log("color " + data);
+        if (data == 0) {
+            return '#463E3E';
+        }
+        if (data == 1) {
+            return '#73A15D';
+        }
+        if (data == 2) {
+            return '#BADA55';
+        }
+        if (data == 3) {
+            return '#C2B280';
+        }
+        return '#336699';
+    }
+
+    private defaultPolygon() {
+        return this.createPolygon(true, 0);
+    }
+
+    private createPolygon(bool: boolean, ttype: number) {
+        return {
+            strokeOpacity: 1,
+            strokeWeight: 5,
+            fillOpacity: 0.4,
+            fillColor: this.getColor(ttype),
+            strokeColor: this.getColor(ttype),
+            polyDraggable: bool,
+            editable: bool,
+            clickable: bool,
+            terrainType: ttype,
+            zIndex: ttype,
+            paths: []
+        }
+    }
+
 }
+
+interface polygon {
+    strokeOpacity: number;
+    strokeWeight: number;
+    fillOpacity: number;
+    fillColor: string;
+    strokeColor: string;
+    polyDraggable: boolean;
+    editable: boolean;
+    clickable: boolean;
+    terrainType: number;
+    zIndex: number;
+    paths: Array<LatLngLiteral>;
+} 
